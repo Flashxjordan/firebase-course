e2bfa3732e5a7cf03c2c12adde072b91227e7b90
David Jordan <wordnerd92@gmail.com>
2023-08-01 17:46:33 -0500

    installed google auth provider

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -20,4 +20,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const googleAuthProvider = new GoogleAuthProvider();
 //const analytics = getAnalytics(app);
